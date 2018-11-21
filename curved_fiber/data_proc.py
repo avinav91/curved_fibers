aaf128c95a1ad1aae83eb6516e672b486e35f70b
Avin Avijay <avinkav@uw.edu>
2018-11-21 08:00:05 -0500

    Edits using new code for calculating mid matrix from prof. Mayes original code. More code to edit

--- curved_fiber/data_proc.py
+++ curved_fiber/data_proc.py
@@ -4,12 +4,25 @@
 """
 data_proc.py
 Obtaining coordinates for fiber paths and calculating fiber boundaries and plotting
+- Reads csv data of fiber paths
+-calculates mid angle in x-direction for FEA calculations
+-plot fiber paths
 
-Handles the primary functions
 """
 
+
+from __future__ import print_function
 import sys
-import argparse
+from argparse import ArgumentParser
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+SUCCESS = 0
+INVALID_DATA = 1
+IO_ERROR = 2
+
+DEFAULT_DATA_FILE_NAME = 'Ply_1_Left.csv'
 
 
 def warning(*objs):
@@ -17,27 +30,26 @@ def warning(*objs):
     print("WARNING: ", *objs, file=sys.stderr)
 
 
-def canvas(with_attribution=True):
+def data_analysis(data_array):
     """
-    Placeholder function to show example docstring (NumPy format)
-
-    Replace this function and doc string for your own project
+    Finds the co-ordinates to place middle matrix region for future FEA modeling, calculates angle at the middle
 
     Parameters
     ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
+    data_array : numpy array of y co-ordinates (col-1) and x co-ordinates (col-2) of left boundary of a fiber
+    placement machine run
 
     Returns
     -------
-    quote : str
-        Compiled string including quote and optional attribution
+    data_xLMid : numpy array with middle matrix co-ordinates
     """
+    num_points, num_fibers = data_array.shape
+    xLMidMtrx=np.zeros((num_points-1,num_fibers))
+    for i in range(num_fibers):
+        for j in range(1, num_points):
+            data_xLMid[j-1][i]=(dara_array[j][i]+dara_array[j-1][i])*0.5
 
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
+    return data_xLMid
 
 
 def parse_cmdline(argv):
@@ -49,28 +61,73 @@ def parse_cmdline(argv):
         argv = sys.argv[1:]
 
     # initialize the parser object:
-    parser = argparse.ArgumentParser()
-    # parser.add_argument("-i", "--input_rates", help="The location of the input rates file",
-    #                     default=DEF_IRATE_FILE, type=read_input_rates)
-    parser.add_argument("-n", "--no_attribution", help="Whether to include attribution",
-                        action='store_false')
+    parser = ArgumentParser(description='Reads in a csv (no header) of x,y coordinates and gets the mean of x for  '
+                                        'middle matrix region. There must be the same number of values in each row.')
+    parser.add_argument("-m", "--csv_data_file", help="The location (directory and file name) of the csv file with "
+                                                      "data to analyze",
+                        default=DEFAULT_DATA_FILE_NAME)
     args = None
     try:
         args = parser.parse_args(argv)
+        args.csv_data = np.loadtxt(fname=args.csv_data_file, delimiter=',')
     except IOError as e:
         warning("Problems reading file:", e)
         parser.print_help()
-        return args, 2
+        return args, IO_ERROR
+    except ValueError as e:
+        warning("Read invalid data:", e)
+        parser.print_help()
+        return args, INVALID_DATA
 
-    return args, 0
+    return args, SUCCESS
+
+
+def plot_stats(f_name,data_xLMid):
+    """
+    Makes a plot of middle matrix paths
+    :param data_xLMid: numpy array middle matrix co-ordinates
+    :param f_name: File name to save to
+    :return: saves a png file
+    """
+    num_midpoints, num_fibers = data_stats.shape
+    plt.figure()
+    plt.xlim(-10.0,10.0)
+    plt.ylim(-10.0,10.0)
+    for i in range(numfibers-1)
+    # red dashes, blue squares and green triangles
+    plt.plot(data_xLMid[:,i+1], data_xLMid[:,0])
+    plt.title('Sample path of matrix around fibers')
+    plt.xlabel('X co-ordinate values')
+    plt.ylabel('Y co-ordinate values')
+    out_name = f_name + ".png"
+    plt.savefig(out_name)
+    print("Wrote file: {}".format(out_name))
 
 
 def main(argv=None):
     args, ret = parse_cmdline(argv)
-    if ret != 0:
+    if ret != SUCCESS:
         return ret
-    print(canvas(args.no_attribution))
-    return 0  # success
+    data_xLMid = data_analysis(args.csv_data)
+    '''
+    This section is to identify the input file name and create the output file name
+    '''
+    # get the name of the input file without the directory it is in, if one was specified
+    base_out_fname = os.path.basename(args.csv_data_file)
+    # get the first part of the file name (omit extension) and add the suffix
+    base_out_fname = os.path.splitext(base_out_fname)[0] + '_MidMtrx'
+    # add suffix and extension
+    out_fname = base_out_fname + '.csv'
+
+    ''' 
+    Saves the data to the file name obtained
+    '''
+    np.savetxt(out_fname, data_xLMid, delimiter=',')
+    print("Wrote file: {}".format(out_fname))
+
+    # send the base_out_fname and data to a new function that will plot the data
+    plot_stats(base_out_fname, data_xLMid)
+    return SUCCESS  # success
 
 
 if __name__ == "__main__":
